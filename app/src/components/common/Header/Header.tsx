a38148e39b7dde5fc866bba3b1cd28dced6d3b35
Kim Jae Hyeok <master_catch@naver.com>
2026-05-15 18:13:11 +0900

    fix : Header 디자인 수정 및 맨 하단 메뉴 추가

--- app/src/components/common/Header/Header.tsx
+++ app/src/components/common/Header/Header.tsx
@@ -1,220 +1,199 @@
-"use client";
-
-import React, { useState, useCallback } from "react";
+import { useEffect, useState, useCallback } from "react";
 import Link from "next/link";
+import Image from 'next/image';
+import { FolderOpen } from 'lucide-react';
 
 import styles from "./Header.module.css";
 
-export default function Header() {
-    const [isOpen, setIsOpen] = useState(false);
+import CommonModal from '../Modal/Modal';
 
+interface Props {
+    variant?: 'main' | 'detail';
+}
+
+export default function Header({
+    variant = 'main',
+}: Props) {
+
+    // 1. 햄버거 메뉴 상태 관리
+    const [isOpen, setIsOpen] = useState(false);
     const toggleMenu = useCallback(() => {
         setIsOpen((prev) => !prev);
     }, []);
 
+    // 2. 타이핑 효과
+    const text = "안녕하세요! 풀스택 개발자, Jay입니다😊";
+    const [displayText, setDisplayText] = useState("");
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        if (index < text.length) {
+        const timeout = setTimeout(() => {
+            setDisplayText((prev) => prev + text.charAt(index));
+            setIndex((prev) => prev + 1);
+        }, 70);
+
+        return () => clearTimeout(timeout);
+        }
+    }, [index, text]);
+
+    // 3. 모달창 띄우기
+    const [open, setOpen] = useState(false);
+
+    const handlePreparePage = (
+        e: React.MouseEvent<HTMLAnchorElement>
+    ) => {
+        e.preventDefault();
+
+        setOpen(true);
+    };
+
     return (
-        <header className={styles.header}>
-            <div className={styles.inner}>
-                {/* Header - Title */}
-                <div className={styles.headerTitle}>
-                    <h1 className={styles.headerLogo}>
-                        <Link
-                            href="/home"
-                            className={styles.siteMaintitle}
-                        >
-                            Jstory
-                        </Link>
+        <>
+            <header
+                className={
+                    variant === 'detail'
+                        ? `${styles.header} ${styles.detailHeader}`
+                        : styles.header
+                }
+            >
+                <div className={styles.inner}>
 
-                        <span className={styles.siteSubtitle}>
-                            {" "}
-                            - Jay's Healing Space
-                        </span>
-                    </h1>
-                </div>
+                    {/* Header - Title */}
+                    <div className={styles.headerTitle}>
+                        <h1 className={styles.headerLogo}>
+                            <Link 
+                                href="/home" 
+                                className={styles.siteMaintitle}>
+                                    Jstory
+                            </Link>
 
-                {/* Header - HamburgerMenu */}
-                <button
-                    className={styles.hamburgerMenu}
-                    onClick={toggleMenu}
-                    aria-label="메뉴 열기"
-                    aria-expanded={isOpen}
-                    type="button"
-                >
-                    <div className={styles.menuIcon}>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </button>
-
-                <div
-                    className={`${styles.sideMenu} ${isOpen ? styles.open : ""}`}
-                >
-                    <div className={styles.loginState}>
-                        <h3 className={styles.noLogin}>
-                            로그인이 필요합니다
-                        </h3>
-
-                        <h3 className={styles.yesLogin}>
-                            어서오세요
-                        </h3>
+                            <span className={styles.siteSubtitle}>
+                                {" "} - Jay's Healing Space
+                            </span>
+                        </h1>
                     </div>
 
-                    <ul className={styles.menuSection}>
-                        <li>
-                            <Link href="/">Home</Link>
-                        </li>
+                    {/* Header - HamburgerMenu */}
+                    <button
+                        className={styles.hamburgerMenu}
+                        onClick={toggleMenu}
+                        aria-label="메뉴 열기"
+                        aria-expanded={isOpen}
+                    >
+                        <div className={styles.menuIcon}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </button>
 
-                        <li>
-                            <Link href="/profile">
-                                About Me
-                            </Link>
+                    <div className={`${styles.sideMenu} ${isOpen ? styles.open : ""}`}>
 
-                            <ul className={styles.subSection}>
-                                <li>
-                                    <Link href="/profile">
-                                        Profile
-                                    </Link>
-                                </li>
-
-                                <li>
-                                    <Link href="/project">
-                                        Project
-                                    </Link>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <Link href="/study">
-                                Jay's 공부방
-                            </Link>
-
-                            <ul className={styles.subSection}>
-                                <li>
-                                    <Link href="/server-create">
-                                        리눅스 서버구축하기
-                                    </Link>
-                                </li>
-
-                                <li>
-                                    <Link href="/certificate">
-                                        공부방(리눅스+정처기)
-                                    </Link>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <Link href="/hobby">
-                                취미 기록방
-                            </Link>
-
-                            <ul className={styles.subSection}>
-                                <li>
-                                    <Link href="/mountain">
-                                        등산
-                                    </Link>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <Link href="/three">
-                                3D 공간으로 초대합니다!
-                            </Link>
-                        </li>
-                    </ul>
-
-                    <ul className={styles.bottomMenu}>
-                        <li>
-                            <Link href="/todo">
-                                TIL/WIL
-                            </Link>
-                        </li>
-
-                        <li>
-                            <a
-                                href="https://github.com/tuy112/Jstory"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                Jstory ASIS
-                            </a>
-                        </li>
-
-                        <li>
-                            <a
-                                href="https://jstoryjay.notion.site/Jay-UI-UX-124211b2c60980b98d93f25d148bdcc3?pvs=4"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                노션 이력서 링크
-                            </a>
-                        </li>
-
-                        <li>
-                            <a
-                                href="/doc/Jstory_고도화프로젝트_화면설계서_v0.26_250115.pptx"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                화면설계서 v0.26
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-
-                {/* Header loginMenu */}
-                <div className={styles.loginButtons}>
-                    <div className={styles.webLogin}>
-                        <div className={styles.mainButton}>
-                            로그인메뉴
+                        {/* 메뉴 프로필 */}
+                        <div className={styles.hamburgerProfile}>
+                        <Image
+                            className={styles.profileImage}
+                            src="/images/pika.png"
+                            alt="profileImg"
+                            width={100}
+                            height={100}
+                        />
+                            <h2 className={styles.profileName}>JAY</h2>
+                            <span className={styles.profileDescription}>
+                                {displayText}
+                            </span>
                         </div>
 
-                        <div className={styles.subButtons}>
-                            <Link
-                                href="/login"
-                                className={styles.subButton}
-                            >
-                                로그인
-                            </Link>
+                        <ul className={styles.menuSection}>
+                            <li>
+                                <Link href="/home">Home</Link>
+                            </li>
+                            <li>
+                                <Link href="/profile" onClick={handlePreparePage}>About Me</Link>
+                            </li>
+                            <li>
+                                <Link href="/project" onClick={handlePreparePage}>Project</Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href="https://www.notion.so/jstoryjay/RMS-196211b2c60980a7baccd0751dd9a093"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    기록관리시스템 메모장
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href="https://tuy112.github.io"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    과거의 JSTORY (React)
+                                </Link>
+                            </li>
+                        </ul>
 
-                            <Link
-                                href="/signup"
-                                className={styles.subButton}
-                            >
-                                회원가입
-                            </Link>
+                        {/* 작은 메뉴 영역 */}
+                        <ul className={styles.bottomMenu}>
+                            <li>
+                                <Link href="/guestbook" onClick={handlePreparePage}>방명록</Link>
+                            </li>
+
+                            <li>
+                                <Link href="/gallery" onClick={handlePreparePage}>갤러리</Link>
+                            </li>
+
+                            <li>
+                                <Link href="/music" onClick={handlePreparePage}>Music</Link>
+                            </li>
+
+                            <li>
+                                <Link href="/setting" onClick={handlePreparePage}>설정</Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    {/* Header - topmenu */}
+                    <div className={styles.topMenu}>
+                        <div className={styles.projectButtons}>
+                            <div className={styles.webLogin}>
+                                
+                                <div className={styles.mainButton}>
+                                    <span className={styles.buttonText}>
+                                        프로젝트 바로가기
+                                    </span>
+
+                                    <FolderOpen
+                                        size={18}
+                                        className={styles.mobileProjectIcon}
+                                    />
+                                </div>
+
+                                <div className={styles.subButtons}>
+                                    <Link href="/study-room" className={styles.subButton}>정보처리기사 공부방</Link>
+                                    <Link href="http://192.168.0.36:8088/softkPrototype.jsp#/" className={styles.subButton}>소프트케이 홈페이지 구축현장 (임시배포)</Link>
+                                    <Link href="/mountain" className={styles.subButton} onClick={handlePreparePage}>산행기록방 (미구현)</Link>
+                                    <Link href="/three" className={styles.subButton} onClick={handlePreparePage}>3D공간 (미구현)</Link>
+                                    <Link href="/book" className={styles.subButton} onClick={handlePreparePage}>
+                                        '끌림의 코드 : 지배의 법칙' 책 읽기 (미구현)
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
-                    <div className={styles.mobileLogin}>
-                        <Link
-                            href="/login"
-                            className={styles.mainButton}
-                        >
-                            LOGIN
-                        </Link>
-
-                        <div className={styles.subButtons}>
-                            <Link
-                                href="/signup"
-                                className={styles.subButton}
-                            >
-                                회원가입
-                            </Link>
-
-                            <Link
-                                href="/"
-                                className={styles.subButton}
-                            >
-                                Gate 이동
-                            </Link>
-                        </div>
-                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+
+            <CommonModal
+                open={open}
+                onClose={() => setOpen(false)}
+                title="안내"
+                message={`아직 미구현 페이지입니다. 
+                    빠른 시일 내 화면 개발 완료하겠습니다 :)`}
+            />
+        </>
     );
 }
