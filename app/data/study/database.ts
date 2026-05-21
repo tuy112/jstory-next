166a9abe2f137691451475987fa7bc1b51d3d21d
Kim Jae Hyeok <master_catch@naver.com>
2026-05-22 08:21:18 +0900

    feat : 3,4과목 데이터 빈공간 추가

---
+++ app/data/study/database.ts
@@ -0,0 +1,26 @@
+import { getTodayDate } from '../utils/getTodayDate';
+
+export const databaseData = [
+    {
+        title: '정규화',
+        subject: '데이터베이스 구축',
+        createdAt: getTodayDate(),
+        content: `
+정규화(Normalization)
+
+목적 :
+데이터 중복 제거
+무결성 유지
+이상현상 방지
+
+1정규형 :
+원자값만 저장
+
+2정규형 :
+부분 함수 종속 제거
+
+3정규형 :
+이행 함수 종속 제거
+        `,
+    },
+];
